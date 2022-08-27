bcff786f074321538748db426e713226db87efe7
FelipeCarrascoH <fcarrasco@dhemax.cl>
2022-08-26 22:09:43 -0400

    node

--- Index.js
+++ Index.js
@@ -11,43 +11,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const client = new Client({
-	user: process.env.USER_DB,
-	host: process.env.HOST,
-	database: process.env.DB_NAME,
-	password: process.env.USER_PASS,
-	port: process.env.DB_PORT,
+  user: process.env.USER_DB,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+  password: process.env.USER_PASS,
+  port: process.env.DB_PORT,
 });
 client.connect();
 
 app.get("/allorgs", (req, res) => {
-	client.query("SELECT * FROM orgs", (error, results) => {
-		if (error) {
-			res.json({
-				status: "500",
-				message: "postgres error?",
-				data: error,
-			});
-			return;
-		}
-		if (!results) {
-			res.json({ status: "204", message: "No data", data: [] });
-			return;
-		}
-		res.json({ status: "200", message: "succesfull", data: results.rows });
-	});
-	
+  client.query("SELECT * FROM orgs", (error, results) => {
+    if (error) {
+      res.json({
+        status: "500",
+        message: "postgres error?",
+        data: error,
+      });
+      return;
+    }
+    if (!results) {
+      res.json({ status: "204", message: "No data", data: [] });
+      return;
+    }
+    res.json({ status: "200", message: "succesfull", data: results.rows });
+  });
 });
 app.post("/insertorg", (req, res) => {
-	let user = req.body.user;
-	connection.query(
-		"INSERT INTO orgs (user,created_at) VALUES ( '" + user + "',NOW())",
-		(error, results) => {
-			if (error) console.log(error);
-			res.json("done");
-		}
-	);
+  let user = req.body.user;
+  let created_at = req.body.created_at;
+  client.query(
+    "INSERT INTO orgs (user_name,created_at) VALUES ( '" + user + "','" + created_at + "' ) ",
+    (error, results) => {
+      if (error) {
+        res.json({ status: "500", message: "postgres error?", data: error });
+        return;
+      }
+      res.json({ status: "200", message: "succesfull", data: { results } });
+    }
+  );
 });
 
 app.listen(process.env.PORT, () => {
-	console.log(`App listening on port ${process.env.PORT}`);
+  console.log(`App listening on port ${process.env.PORT}`);
 });
